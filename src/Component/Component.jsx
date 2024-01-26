e3b90c91572f527d7b7e6229e383e93dd3b9b5e8
Muskan Koshti <koshtichicku1399@gmail.com>
2024-01-26 18:46:40 +0530

    first commit

---
+++ src/Component/Component.jsx
@@ -0,0 +1,29 @@
+import './Component.css'
+import React from 'react'
+
+const Component = () => {
+  return (
+    <div className="top">
+        <div className="left-compo">
+            <h1 className="first">SEO</h1>
+            <h1 className="second">PACKAGES</h1>
+            <p className="para"> <br/>Today, a website cannot do without search engine optimization. The strategy is the lifeline to bring you on the first page and rank higher than your competitors. You can no longer sign up for Yoast SEO or subscribe to YEXT and expect results. It is time for a customized and evolved approach thanks to the updates in search engine optimization.<br /><br/>
+
+                We provide expert SEO services for small, medium, and enterprise-level companies.<br/> If you're looking for SEO packages that deliver, discover what we have in store for you.</p>
+        </div>
+        <div className="right-compo">
+            <h2 >REGULAR MEETINGS</h2>
+            <h2>TECHNICAL SEO</h2>
+            <h2>KEYWORD TRACKING</h2>
+            <h2>LOCAL SEO</h2>
+            <h2>MULTI-LOCATION SEO</h2>
+            <h2>REVIEW ACCELERATION</h2>
+            <h2>LOCAL SEO</h2>
+            <h2>COPY WRITING</h2>
+            <h2>LINK BUILDING</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Component
